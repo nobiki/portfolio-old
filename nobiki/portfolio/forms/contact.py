15cc0cda435f3be1e536095b6b7991351294e838
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-03 20:53:10 +0900

    send email

--- nobiki/portfolio/forms/contact.py
+++ nobiki/portfolio/forms/contact.py
@@ -27,6 +27,6 @@ class ContactForm(forms.Form):
         subject         = self.cleaned_data['name']
         message         = self.cleaned_data['message']
         from_address    = self.cleaned_data['email']
-        to_address      = os.environ.get('PORTFOLIO_ENV_DESTINATION_CONTACT')
+        to_address      = [os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT")]
 
         send_mail(subject, message, from_address, to_address)
