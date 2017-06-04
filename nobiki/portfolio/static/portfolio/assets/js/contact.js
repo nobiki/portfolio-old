a916e55e3d2625f86f8175496de8cc669fb5867c
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-04 23:44:13 +0900

    modified:   portfolio/static/portfolio/assets/js/contact.js

--- nobiki/portfolio/static/portfolio/assets/js/contact.js
+++ nobiki/portfolio/static/portfolio/assets/js/contact.js
@@ -12,16 +12,24 @@ $("form[data-contact='form']").submit(function(e){
         dataType: 'json',
         contentType: false,
         processData: false,
-        beforeSend: function(){}
+        beforeSend: function(){
+            $("#contact").find("input[type='submit']").attr("disabled","disabled");
+            $("#contact").find("input[type='submit']").fadeOut();
+        }
     })
     .always(function(data,status) {
         if("success" == status)
         {
             $("#contact .box").css("-webkit-transform", "rotateX(180deg)");
             $("#contact .box").css("transform", "rotateX(180deg)");
+        }else{
+            $("#contact").find("input[type='submit']").removeAttr("disabled");
+            $("#contact").find("input[type='submit']").show();
         }
     })
     .fail(function(data) {
+        $("#contact").find("input[type='submit']").removeAttr("disabled");
+        $("#contact").find("input[type='submit']").show();
     });
 
     return false;
