55f2c08503bdd72f6431317dd8a9c48f94c400ee
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-06 23:05:14 +0900

    Websocket test.

---
+++ ws.py
@@ -0,0 +1,12 @@
+import uwsgi
+
+def application(env, start_response):
+    # complete the handshake
+    uwsgi.websocket_handshake(env['HTTP_SEC_WEBSOCKET_KEY'], env.get('HTTP_ORIGIN', ''))
+    uwsgi.websocket_send("hello")
+
+    # while True:
+    #     msg = uwsgi.websocket_recv()
+    #     uwsgi.websocket_send(msg)
+
+    return ['']
