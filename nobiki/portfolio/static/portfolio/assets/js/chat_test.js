d1ddc24f4656f600ad6d0f103ef183dffc860c39
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-08 00:21:58 +0900

    ws test

---
+++ nobiki/portfolio/static/portfolio/assets/js/chat_test.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var s = new WebSocket("ws://ws.9zilla.local/");
+    s.onopen = function(){
+        console.log("[onopen]");
+        s.send("connected!!!");
+    };
+    s.onmessage = function(e) {
+        console.log("[onmessage]");
+        console.log(e.data);
+        $.snackbar({content: e.data});
+    };
+    s.onerror = function(e) {
+        console.log("[onerror]");
+        console.log(e);
+    };
+    s.onclose = function(e) {
+        console.log("[onclose]");
+        console.log("connection closed..");
+
+        console.log("Close Code = " + e.code);
+        console.log("Close Reason = " + e.reason);
+    };
+});
