382a9b085e4769844e623f1789ee6ba348ac7362
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-07 23:19:19 +0900

    new file:   echo.py

--- ws.py
+++ ws.py
@@ -1,12 +1,12 @@
 import uwsgi
+import time
 
 def application(env, start_response):
     # complete the handshake
     uwsgi.websocket_handshake(env['HTTP_SEC_WEBSOCKET_KEY'], env.get('HTTP_ORIGIN', ''))
     uwsgi.websocket_send("hello")
+    uwsgi.websocket_send("hello2")
 
     # while True:
-    #     msg = uwsgi.websocket_recv()
-    #     uwsgi.websocket_send(msg)
-
-    return ['']
+        # msg = uwsgi.websocket_recv_nb()
+        # uwsgi.websocket_send(msg)
