88ab6f15610c6ba2220e34fe4646c7270abc54d1
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-08 00:15:39 +0900

    ws chat test

---
+++ chat.py
@@ -0,0 +1,41 @@
+import uwsgi
+import redis
+import time
+import sys
+
+def application(env, start_response):
+    # complete the handshake
+    uwsgi.websocket_handshake(env['HTTP_SEC_WEBSOCKET_KEY'], env.get('HTTP_ORIGIN', ''))
+
+    r = redis.StrictRedis(host='redis-gandamu', port=6379, db=0)
+    channel = r.pubsub()
+    channel.subscribe('foobar')
+    websocket_fd = uwsgi.connection_fd()
+    redis_fd = channel.connection._sock.fileno()
+
+    while True:
+        uwsgi.wait_fd_read(websocket_fd, 3)
+        uwsgi.wait_fd_read(redis_fd)
+        uwsgi.suspend()
+        fd = uwsgi.ready_fd()
+        if fd > -1:
+            if fd == websocket_fd:
+                msg = uwsgi.websocket_recv_nb()
+                if msg:
+                    r.publish('foobar', msg)
+            elif fd == redis_fd:
+                msg = channel.parse_response()
+                print(msg)
+                print(msg[2])
+                # only interested in user messages
+                t = 'message'
+                if sys.version_info[0] > 2:
+                    t = b'message'
+                if msg[0] == t:
+                    uwsgi.websocket_send("%s" % msg[2])
+                    # uwsgi.websocket_send("[%s] %s" % (time.time(), msg))
+        else:
+            # on timeout call websocket_recv_nb again to manage ping/pong
+            msg = uwsgi.websocket_recv_nb()
+            if msg:
+                r.publish('foobar', msg)
