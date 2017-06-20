b5134ed0f2fc72031bc094447bd2394fc788ea6b
Naoaki Obiki <obiki.wk@gmail.com>
2017-06-20 21:23:15 +0900

    webpack (bundle js,css)

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,8 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: {
-        "common": [
-            path.join(__dirname, "./webpack/common/js/snackbar.min.js"),
-            path.join(__dirname, "./webpack/common/js/swiper.min.js"),
-            path.join(__dirname, "./webpack/common/js/jquery.poptrox.min.js"),
-            path.join(__dirname, "./webpack/common/js/jquery.scrollex.min.js"),
-            path.join(__dirname, "./webpack/common/js/jquery.scrolly.min.js"),
-            path.join(__dirname, "./webpack/common/js/jquery.timelify.min.js"),
-            path.join(__dirname, "./webpack/common/js/jquery.ripples-min.js"),
-            path.join(__dirname, "./webpack/common/js/morphext.min.js"),
-            path.join(__dirname, "./webpack/common/js/skel.min.js"),
-            path.join(__dirname, "./webpack/common/js/util.js"),
-            path.join(__dirname, "./webpack/common/js/big-picture.min.js"),
-            path.join(__dirname, "./webpack/common/js/common.js"),
-        ],
-        "portfolio/contact": path.join(__dirname, "./webpack/portfolio/contact/entry.js")
+        "common": path.join(__dirname, "./webpack/common/entry.js"),
+        "portfolio/contact": path.join(__dirname, "./webpack/portfolio/contact/entry.js"),
     },
     output: {
         filename: "./nobiki/static/[name]/bundle.js"
@@ -28,6 +15,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract({fallback:'style-loader',use:'css-loader'})
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             },
         ]
     },
--- webpack/common/entry.js
+++ webpack/common/entry.js
@@ -1,14 +1,22 @@
+require("./js/snackbar.min.js");
+require("./js/swiper.min.js");
 require("./js/jquery.poptrox.min.js");
-require("./js/jquery.ripples-min.js");
 require("./js/jquery.scrollex.min.js");
 require("./js/jquery.scrolly.min.js");
-require("./js/jquery.timelify.js");
 require("./js/jquery.timelify.min.js");
-require("./js/common.js");
+require("./js/jquery.ripples-min.js");
 require("./js/morphext.min.js");
 require("./js/skel.min.js");
-require("./js/swiper.min.js");
 require("./js/util.js");
 require("./js/big-picture.min.js");
-require("./js/snackbar.min.js");
+require("./js/common.js");
 
+require("./css/animate.css");
+require("./css/contact.css");
+require("./css/ripple.css");
+require("./css/snackbar.css");
+require("./css/snackbar.theme.material.css");
+require("./css/swiper.css");
+require("./css/timelify.css");
+require("./css/font-awesome.min.css");
+require("./css/big-picture.css");
