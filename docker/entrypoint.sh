35aa47160e93b172264dd85af8a358589b50be6a
Naoaki Obiki <obiki.wk@gmail.com>
2017-12-01 11:41:08 +0900

    modified:   docker/entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -e /bootstrap.lock ]; then
+if [[ ! -e /home/docker/code/app/bootstrap.lock ]]; then
 	git clone https://github.com/nobiki/portfolio.git /home/docker/code/app
 	pip3 install -r /home/docker/code/app/requirements.txt
 
@@ -8,7 +8,7 @@ if [ ! -e /bootstrap.lock ]; then
 	cd /home/docker/code/app && python3 nobiki/manage.py collectstatic --noinput
 	cd /home/docker/code/app && ./node_modules/.bin/webpack -p
 
-	touch /bootstrap.lock
+	touch /home/docker/code/app/bootstrap.lock
 else
 	cd /home/docker/code/app && ./.jenkins
 fi
