59452e86ef06c555a426f6369e23bc0e3555d402
Naoaki Obiki <obiki.wk@gmail.com>
2017-12-01 13:21:51 +0900

    modified:   docker/nginx-app.conf

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -e /home/docker/code/app/bootstrap.lock ]]; then
+if [[ ! -e /bootstrap.lock ]]; then
 	git clone https://github.com/nobiki/portfolio.git /home/docker/code/app
 	pip3 install -r /home/docker/code/app/requirements.txt
 
@@ -8,7 +8,7 @@ if [[ ! -e /home/docker/code/app/bootstrap.lock ]]; then
 	cd /home/docker/code/app && python3 nobiki/manage.py collectstatic --noinput
 	cd /home/docker/code/app && ./node_modules/.bin/webpack -p
 
-	touch /home/docker/code/app/bootstrap.lock
+	touch /bootstrap.lock
 else
 	cd /home/docker/code/app && ./.jenkins
 fi
