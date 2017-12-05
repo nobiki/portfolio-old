927598801f895d8e7a7efdfaeb998320d9afcd87
Naoaki Obiki <obiki.wk@gmail.com>
2017-12-05 13:11:41 +0900

    modified:   nobiki/portfolio/forms/contact.py

--- nobiki/portfolio/forms/contact.py
+++ nobiki/portfolio/forms/contact.py
@@ -1,7 +1,11 @@
 import os
-import requests
 from django import forms
+
 # from django.core.mail import send_mail
+
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import sendgrid
 
 class ContactForm(forms.Form):
     name = forms.CharField(
@@ -29,20 +33,19 @@ class ContactForm(forms.Form):
     g_recaptcha_response = forms.CharField(required=False)
 
     def send_email(self):
-        # subject         = self.cleaned_data['name']
-        # message         = self.cleaned_data['message']+"\n\n\n\n\n-------------------------------------\n\nPortfolio から送信\n"
-        # from_address    = self.cleaned_data['email']
-        # to_address      = [os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT")]
+        subject         = self.cleaned_data['name']
+        message         = self.cleaned_data['message']+"\n\n\n\n\n-------------------------------------\n\nPortfolio から送信\n"
+        from_address    = self.cleaned_data['email']
+        to_address      = [os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT")]
 
         # send_mail(subject, message, from_address, to_address)
 
-        headers = {
-                "Authorization": "Bearer "+os.environ.get("PORTFOLIO_ENV_EMAIL_API_KEY")
-                }
-        payload = {
-                "to": os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT"),
-                "from": self.cleaned_data['email'],
-                "subject": self.cleaned_data['name'],
-                "html": self.cleaned_data['message']+"\n\n\n\n\n-------------------------------------\n\nPortfolio から送信\n"
-                }
-        r = requests.post(os.environ.get("PORTFOLIO_ENV_EMAIL_API_URL"), headers=headers params-payload)
+		client = sendgrid.SendGridClient(os.environ.get("PORTFOLIO_ENV_EMAIL_API_KEY"))
+		message = sendgrid.Mail()
+
+		message.add_to(to_address)
+		message.set_from(from_address)
+		message.set_subject(subject)
+		message.set_html(message)
+
+		client.send(message)
