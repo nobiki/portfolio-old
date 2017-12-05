4ec32a3fe85333d411f29591edc32414dffdcdb8
Naoaki Obiki <obiki.wk@gmail.com>
2017-12-05 12:02:35 +0900

    modified:   nobiki/portfolio/forms/contact.py

--- nobiki/portfolio/forms/contact.py
+++ nobiki/portfolio/forms/contact.py
@@ -1,6 +1,7 @@
 import os
+import requests
 from django import forms
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 
 class ContactForm(forms.Form):
     name = forms.CharField(
@@ -28,9 +29,20 @@ class ContactForm(forms.Form):
     g_recaptcha_response = forms.CharField(required=False)
 
     def send_email(self):
-        subject         = self.cleaned_data['name']
-        message         = self.cleaned_data['message']+"\n\n\n\n\n-------------------------------------\n\nPortfolio から送信\n"
-        from_address    = self.cleaned_data['email']
-        to_address      = [os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT")]
+        # subject         = self.cleaned_data['name']
+        # message         = self.cleaned_data['message']+"\n\n\n\n\n-------------------------------------\n\nPortfolio から送信\n"
+        # from_address    = self.cleaned_data['email']
+        # to_address      = [os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT")]
 
-        send_mail(subject, message, from_address, to_address)
+        # send_mail(subject, message, from_address, to_address)
+
+        headers = {
+                "Authorization": "Bearer "+os.environ.get("PORTFOLIO_ENV_EMAIL_API_KEY")
+                }
+        payload = {
+                "to": os.environ.get("PORTFOLIO_ENV_DESTINATION_CONTACT"),
+                "from": self.cleaned_data['email'],
+                "subject": self.cleaned_data['name'],
+                "html": self.cleaned_data['message']+"\n\n\n\n\n-------------------------------------\n\nPortfolio から送信\n"
+                }
+        r = requests.post(os.environ.get("PORTFOLIO_ENV_EMAIL_API_URL"), headers=headers params-payload)
